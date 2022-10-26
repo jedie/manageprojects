0a4c2e732ccce9b3fe963405516dbb9c2e05e17c
JensDiemer <git@jensdiemer.de>
2022-10-26 18:39:54 +0200

    enhance log setup

--- manageprojects/log_utils.py
+++ manageprojects/log_utils.py
@@ -1,12 +1,14 @@
 import logging
 
 
-def log_config(level=logging.DEBUG, format='%(asctime)s %(levelname)s | %(message)s'):
+def log_config(force=True, level=logging.DEBUG, format='%(asctime)s %(levelname)s | %(message)s'):
     logger = logging.getLogger('manageprojects')
-    logger.setLevel(level)
+    is_configured = logger.handlers and logger.level
+    if force or not is_configured:
+        logger.setLevel(level)
 
-    ch = logging.StreamHandler()
-    ch.setLevel(level)
-    ch.setFormatter(logging.Formatter(format))
+        ch = logging.StreamHandler()
+        ch.setLevel(level)
+        ch.setFormatter(logging.Formatter(format))
 
-    logger.addHandler(ch)
+        logger.addHandler(ch)
